320426c360d8cc03f879c938978c11de49c79335
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-07-23 18:22:16 -0400

    No stretch for me today, my brain is ready to tap out.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,17 +1,39 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const first = "Dog";
+
+function combine() {
+  const second = "gone";
+  console.log(`I'm sick of these ${first}${second} whipersnappers`);
+}
+
+combine();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+
   // Return a function that when invoked increments and returns a counter variable.
-};
+
+  const counter = () => {
+    counter = 0;
+    function newCounter() {
+      return counter += 1;
+    }
+    newCounter();
+    newCounter();
+    newCounter();
+    console.log(counter);
+  }
+
+  // Don't think that's right as it says the variable is undefined (???), but I'm mentally exhausted and it's all I've got
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+// I couldn't get the first stretch to work, so I'm not even gonna try this one lol.
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
